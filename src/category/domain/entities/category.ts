49d4567ff53b43d69a39440632ba27a8b0f0d4e5
Matheus Lino <matheuslino86@gmail.com.br>
2023-05-01 21:48:19 -0400

    feat: :sparkles: First commit

---
+++ src/category/domain/entities/category.ts
@@ -0,0 +1,38 @@
+export type CategoryProps = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export class Category {
+  constructor(public readonly props: CategoryProps) {
+    this.props.description = this.description ?? null;
+    this.props.is_active = this.is_active ?? true;
+    this.props.created_at = this.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  private set description(value) {
+    this.props.description = value ?? null;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  private set is_active(value: boolean) {
+    this.props.is_active = value ?? null;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
+}
