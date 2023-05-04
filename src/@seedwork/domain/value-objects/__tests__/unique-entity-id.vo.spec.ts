9c453b2fa2f3c44a6f5dbb2e9e869440f0ca0de2
Matheus Lino <matheuslino86@gmail.com.br>
2023-05-04 17:58:17 -0400

    feat: :sparkles: Object and value object test

--- src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,5 +1,5 @@
-import InvalidUuidError from "../../@seedwork/errors/invalid-uuid.error";
-import UniqueEntityId from "./unique-entity-id.vo";
+import InvalidUuidError from "../../../errors/invalid-uuid.error";
+import UniqueEntityId from "./../unique-entity-id.vo";
 import { validate as uuidValidate } from "uuid";
 
 function spyValidateMethod() {
@@ -27,14 +27,14 @@ describe("UniqueEntityId Unit Tests", () => {
     const validateSpy = spyValidateMethod();
     const uuid = "3936900b-92c8-4446-b1d7-1933eeaec6fa";
     const vo = new UniqueEntityId(uuid);
-    expect(vo.id).toBe(uuid);
+    expect(vo.value).toBe(uuid);
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it("should accept a uuid in constructor", () => {
     const validateSpy = spyValidateMethod();
     const vo = new UniqueEntityId();
-    expect(uuidValidate(vo.id)).toBeTruthy();
+    expect(uuidValidate(vo.value)).toBeTruthy();
     expect(validateSpy).toHaveBeenCalled();
   });
 });
